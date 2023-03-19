ea7fe5e65d4f3f1dc13838dc0396d34a4a439559
fernandochirico <fernandochirico@yahoo.com.ar>
2023-03-18 23:57:41 -0300

    a

---
+++ src/main/java/group/tp/Estructura_Pronostico.java
@@ -0,0 +1,99 @@
+
+package group.tp;
+
+/**
+ *
+ * @author GRUPO 13
+ */
+
+import com.opencsv.bean.CsvBindByPosition;
+import com.opencsv.bean.CsvDate;
+import java.time.LocalDate;
+
+public class Estructura_Pronostico {
+   
+    @CsvBindByPosition(position = 0)
+    private Integer p_idRonda;
+    @CsvBindByPosition(position = 1)
+    private Integer p_idPronostico;
+    @CsvBindByPosition(position = 2)
+    private Integer p_idPartido;
+    @CsvBindByPosition(position = 3)
+    private Integer p_idEquipo1;
+    @CsvBindByPosition(position = 4)
+    private Integer p_idEquipo2;
+    @CsvBindByPosition(position = 5)
+    private char p_gana1;
+      @CsvBindByPosition(position = 6)
+    private char p_empata;
+        @CsvBindByPosition(position = 7)
+    private char p_gana2;
+
+    public Integer getP_idRonda() {
+        return p_idRonda;
+    }
+
+    public Integer getP_idPronostico() {
+        return p_idPronostico;
+    }
+
+    public Integer getP_idPartido() {
+        return p_idPartido;
+    }
+
+    public Integer getP_idEquipo1() {
+        return p_idEquipo1;
+    }
+
+    public Integer getP_idEquipo2() {
+        return p_idEquipo2;
+    }
+
+    public char getP_gana1() {
+        return p_gana1;
+    }
+
+    public char getP_empata() {
+        return p_empata;
+    }
+
+    public char getP_gana2() {
+        return p_gana2;
+    }
+
+    public void setP_idRonda(Integer p_idRonda) {
+        this.p_idRonda = p_idRonda;
+    }
+
+    public void setP_idPronostico(Integer p_idPronostico) {
+        this.p_idPronostico = p_idPronostico;
+    }
+
+    public void setP_idPartido(Integer p_idPartido) {
+        this.p_idPartido = p_idPartido;
+    }
+
+    public void setP_idEquipo1(Integer p_idEquipo1) {
+        this.p_idEquipo1 = p_idEquipo1;
+    }
+
+    public void setP_idEquipo2(Integer p_idEquipo2) {
+        this.p_idEquipo2 = p_idEquipo2;
+    }
+
+    public void setP_gana1(char p_gana1) {
+        this.p_gana1 = p_gana1;
+    }
+
+    public void setP_empata(char p_empata) {
+        this.p_empata = p_empata;
+    }
+
+    public void setP_gana2(char p_gana2) {
+        this.p_gana2 = p_gana2;
+    }
+     
+        
+    
+}
+
---
+++ src/main/java/group/tp/Estructura_Resultado.java
@@ -0,0 +1,113 @@
+package group.tp;
+
+import com.opencsv.bean.CsvBindByPosition;
+
+/**
+ *
+ * @author GRUPO 13
+ */
+public class Estructura_Resultado {
+
+    @CsvBindByPosition(position = 0)
+    private Integer r_idRonda;
+    @CsvBindByPosition(position = 1)
+    private Integer r_idPartido;
+    @CsvBindByPosition(position = 2)
+    private Integer r_idequipo1;
+    @CsvBindByPosition(position = 3)
+    private String r_equipo1Nombre;
+    @CsvBindByPosition(position = 4)
+    private String r_equipo1Descripcion;
+    @CsvBindByPosition(position = 5)
+    private Integer r_equipo1Goles;
+    @CsvBindByPosition(position = 6)
+    private Integer r_equipo2Goles;
+    @CsvBindByPosition(position = 7)
+    private Integer r_idequipo2;
+    @CsvBindByPosition(position = 8)
+    private String r_equipo2Nombre;
+    @CsvBindByPosition(position = 9)
+    private String r_equipo2Descripcion;
+
+    public Integer getR_idRonda() {
+        return r_idRonda;
+    }
+
+    public Integer getR_idPartido() {
+        return r_idPartido;
+    }
+
+    public Integer getR_idequipo1() {
+        return r_idequipo1;
+    }
+
+    public String getR_equipo1Nombre() {
+        return r_equipo1Nombre;
+    }
+
+    public String getR_equipo1Descripcion() {
+        return r_equipo1Descripcion;
+    }
+
+    public Integer getR_equipo1Goles() {
+        return r_equipo1Goles;
+    }
+
+    public Integer getR_equipo2Goles() {
+        return r_equipo2Goles;
+    }
+
+    public Integer getR_idequipo2() {
+        return r_idequipo2;
+    }
+
+    public String getR_equipo2Nombre() {
+        return r_equipo2Nombre;
+    }
+
+    public String getR_equipo2Descripcion() {
+        return r_equipo2Descripcion;
+    }
+
+    public void setR_idRonda(Integer r_idRonda) {
+        this.r_idRonda = r_idRonda;
+    }
+
+    public void setR_idPartido(Integer r_idPartido) {
+        this.r_idPartido = r_idPartido;
+    }
+
+    public void setR_idequipo1(Integer r_idequipo1) {
+        this.r_idequipo1 = r_idequipo1;
+    }
+
+    public void setR_equipo1Nombre(String r_equipo1Nombre) {
+        this.r_equipo1Nombre = r_equipo1Nombre;
+    }
+
+    public void setR_equipo1Descripcion(String r_equipo1Descripcion) {
+        this.r_equipo1Descripcion = r_equipo1Descripcion;
+    }
+
+    public void setR_equipo1Goles(Integer r_equipo1Goles) {
+        this.r_equipo1Goles = r_equipo1Goles;
+    }
+
+    public void setR_equipo2Goles(Integer r_equipo2Goles) {
+        this.r_equipo2Goles = r_equipo2Goles;
+    }
+
+    public void setR_idequipo2(Integer r_idequipo2) {
+        this.r_idequipo2 = r_idequipo2;
+    }
+
+    public void setR_equipo2Nombre(String r_equipo2Nombre) {
+        this.r_equipo2Nombre = r_equipo2Nombre;
+    }
+
+    public void setR_equipo2Descripcion(String r_equipo2Descripcion) {
+        this.r_equipo2Descripcion = r_equipo2Descripcion;
+    }
+
+    
+}
