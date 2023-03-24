c59b8ea9c3bfd3f0a34e5c847835bff808ce313d
fernandochirico <fernandochirico@yahoo.com.ar>
2023-03-24 10:03:39 -0300

    a

--- src/main/java/group/tp/Partido.java
+++ src/main/java/group/tp/Partido.java
@@ -5,7 +5,7 @@ package group.tp;
  *
  * @author GRUPO 13
  */
-public class Partido {
+public class Partido extends Equipo{
  
     int idPartido;
     Equipo equipo1 = new Equipo();
@@ -16,11 +16,11 @@ public class Partido {
     public Partido() {
     }
 
-    public Partido(int idPartido, int golesEquipo1, int golesEquipo2) {
+    public Partido(int idPartido, int golesEquipo1, int golesEquipo2, int idEquipo, String nombre, String descripcion) {
+        super(idEquipo, nombre, descripcion);
         this.idPartido = idPartido;
         this.golesEquipo1 = golesEquipo1;
         this.golesEquipo2 = golesEquipo2;
     }
-
     
     }
--- src/main/java/group/tp/Pronostico.java
+++ src/main/java/group/tp/Pronostico.java
@@ -4,7 +4,7 @@ package group.tp;
  *
  * @author GRUPO 13
  */
-public class Pronostico {
+public class Pronostico extends Partido {
     int idPronostico;
     Partido partido = new Partido();
     Equipo equipo1 = new Equipo();
@@ -16,12 +16,15 @@ public class Pronostico {
     public Pronostico() {
     }
 
-    public Pronostico(int idPronostico, char gana1, char empata, char gana2) {
+    public Pronostico(int idPronostico, char gana1, char empata, char gana2, int idPartido, int golesEquipo1, int golesEquipo2, int idEquipo, String nombre, String descripcion) {
+        super(idPartido, golesEquipo1, golesEquipo2, idEquipo, nombre, descripcion);
         this.idPronostico = idPronostico;
         this.gana1 = gana1;
         this.empata = empata;
         this.gana2 = gana2;
     }
+
+
 
     
 }
